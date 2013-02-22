a90f43a46f84bda7296305ad30d4bf608282115d
Pam Suebvisai <sinapam@proteus-tech.com>
2013-02-22 20:24:00 +0700

    added swap_last_to_first function

---
+++ functions.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+def swap_last_to_first(input):
+    return '%s%s' % (input[2], input[:2])
---
+++ tests/test_functions.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase
+
+from functions import swap_last_to_first
+
+class TestSwapLastToFist(TestCase):
+    def test_must_swap_last_to_first_correctly(self):
+        result = swap_last_to_first('abc')
+        self.assertEqual(result, 'cab')
